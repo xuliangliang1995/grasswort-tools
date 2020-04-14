987c328d425e3eda39a9d79bebe4556087c9eb6b
xuliang <835547206@qq.com>
2020-04-14 22:43:00 +0800

    添加JAXB 解析 xml

---
+++ src/main/java/org/grasswort/jaxb/model/Score.java
@@ -0,0 +1,18 @@
+package org.grasswort.jaxb.model;
+
+import lombok.Data;
+
+/**
+ * @author xuliangliang
+ * @Classname Score.java
+ * @Description
+ * @Date 2020/4/14
+ * @blame Java Team
+ */
+@Data
+public class Score {
+
+    private String item;
+
+    private Integer score;
+}
